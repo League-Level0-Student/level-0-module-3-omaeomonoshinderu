669e69fc67e0ece973df971f02c6276e29cb10de
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-30 13:28:01 -0700

    crazy cat lady,

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,18 +5,27 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String answer= JOptionPane.showInputDialog("how many cats do you have?");
 		// 2. Convert their answer into an int
-
+int cats= Integer.parseInt(answer);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+if(cats>3) {
+	JOptionPane.showMessageDialog(null, "Your a crazy cat lady, like daaannng, " + answer + " cats, that's alot!");
+}
 
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+	else if(cats<3 && cats>0) {
+		playVideo("https://www.youtube.com/watch?v=iRXJXaLV0n4");
+	}
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+	else if(cats<0) {	
+		playVideo("https://www.youtube.com/watch?v=F44HRvn8tiA");
+	}
 	}
 
 	static void playVideo(String videoURL) {
