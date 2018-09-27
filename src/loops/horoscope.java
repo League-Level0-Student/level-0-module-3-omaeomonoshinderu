341e314455f90652a219e0431500862931c6d4d6
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-27 13:23:40 -0700

    horoscope dnf

--- src/loops/horoscope.java
+++ src/loops/horoscope.java
@@ -1,5 +1,11 @@
 package loops;
 
-public class horoscope {
+import javax.swing.JOptionPane;
 
+public class horoscope {
+public static void main(String[] args) {
+	String sign = JOptionPane.showInputDialog("What's your star sign?");
+int star = Integer.parseInt(sign);
+
+}
 }
