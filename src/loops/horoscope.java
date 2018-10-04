d47b79a0372f01e696ffa53077788cd1a6afb0b7
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-04 13:26:14 -0700

    horoscope

--- src/loops/horoscope.java
+++ src/loops/horoscope.java
@@ -5,7 +5,76 @@ import javax.swing.JOptionPane;
 public class horoscope {
 public static void main(String[] args) {
 	String sign = JOptionPane.showInputDialog("What's your star sign?");
-int star = Integer.parseInt(sign);
+if (sign .equals("capricorn")) {
+	JOptionPane.showMessageDialog(null,"Capricorn Zodiac Sign\n" + 
+			"Earth sign; ruled by Saturn.\n" + 
+			"Timeless, driven, calculating, ambitious.\n" + 
+			"\n" + 
+			"→ Click here to read your Capricorn horoscope!\n" + 
+			"\n" + 
+			"The mountain goat is the symbol for this sign because of the characteristic of the goat to climb higher and higher without losing his footing. Capricorn people are the type that are goal oriented and driven to succeed despite all odds. They will work long and hard hours toward a purposeful goal. Self-disciplined and successful, it is no wonder that some of the world’s greatest scientists, leaders, and teachers have been Capricorn.\n" + 
+			"\n" + 
+			"Achievement is everything to a Capricorn. For this reason they tend to take life very seriously and are not tolerant of those who do not. They are also very good at state’s craft engaging in mental parrying at a high level. They are natural born leaders, politicians, mathematicians, and diplomats. Capricorn people tend to guard their hearts closely and well. To get close to a Capricorn may take some time, but once achieved, well worth it.\n" + 
+			"\n" + 	 
+			"Upside: Capricorns are the superheroes when It comes to making realistic, logical decisions. Their ability to cut through the red tape and see the bottom line is what makes them a real asset to any relationship or occupation where teamwork is valued. Capricorn are family oriented, but will keep their distance if the family is perceived to be violent, harmful, or unfulfilling. Capricorn have a dry sense of humor and their sarcastic wit is only appreciated by those who find humor everywhere. Highly intellectual, good with numbers and analysis, this sign is not often loud, but they are most definitely the muscle and the power behind the very successful machine.\n" + 
+			"\n" + 
+			"Downside: Because they are so dedicated to the big picture and the minute details, they can seem dull to others, who don’t understand the active internal nature of the Capricorn. While seeming almost emotionless on the surface, the Capricorn’s mind is always running on overdrive. They can often seem selfish and stingy, keeping information to themselves, or strategically withholding information for their own gain. Often they can be the less imaginative of the signs and refuse to face the facts about a situation because it would signal that they were wrong about something (which they hate).\n" + 
+			"\n" + 
+			"Capricorn is an earth sign, like Virgo and Taurus" );
+}
+else if (sign .equals("aquarius")) {
+	JOptionPane.showMessageDialog(null, "Aquarius Zodiac Sign\n" + 
+			"Air sign; ruled by Uranus.\n" + 
+			"Forward thinking, communicative, people oriented, stubborn, generous, and dedicated.\n" + 
+			"\n" + 
+			"→ Click here to read your Aquarius horoscope!\n" + 
+			"\n" + 
+			"The man who carries the pitcher of water is the symbol for this sign. This is because Aquarians are often generous with their time and resources. For this reason this sign often find themselves in occupations where they are helping on some humanitarian level. They are interested and have a deep concern for the welfare of others. They never know a stranger and are generally loved by all around them. Aquarians never meet a stranger and often will go out of their way to meet with people that they’ve not heard from for years. Connection is at the core of this sign.\n" + 
+			"\n" + 
+			"Fascinated by all things gadget, many Aquarians enjoy tinkering with inventions and processes. On the surface, Aquarius people are often not very emotional, because they are more concerned with the exchanging of ideas and how they might better assist someone in their cause. Aquarians have a great deal of patience and tolerance for their fellow man. They make excellent teachers and relief aid workers, though do not become personally involved with them.\n" + 
+			"\n" + 
+			"Upside: Aquarians are among the friendliest of the signs, making friends everywhere. They are often chosen among co-workers and family for special awards and honors because of their ability to network. Highly intelligent they are energetic and talented, often using their abilities to help others. Despite their extensive people skills, they often crave alone time to reflect and think about things. Meditation is recommended for this sign as it helps to maintain that sense of alone time, while at the same time a feeling of connectedness with something greater than themselves.\n" + 
+			"\n" + 
+			"Downside: If this sign does not have alone time and time to do much desired reflection, they become dour, emotionless, and even are prone to depressive interludes. They can become highly resentful if they believe that their voice is not being truly heard or valued. Because they do self-reflect, this sign can become quite eccentric in some of the hobbies that they pursue, or activities they enjoy.\n" + 
+			"\n" + 
+			"Aquarius is an air sign, like Gemini and Libra");
+} 
+else if (sign .equals("pisces")) {
+	JOptionPane.showMessageDialog(null, "Pisces Zodiac Sign\n" + 
+			"Water sign; ruled by Neptune.\n" + 
+			"Likeable, energetic, passionate, sensitive.\n" + 
+			"\n" + 
+			"→ Click here to read your Pisces horoscope!\n" + 
+			"\n" + 
+			"The fish is the symbol given to this sign, primarily because fish live in a secretive and mysterious place. Pisces tend to be daydreamers, often dreaming up brilliant ideas and plans which they are unable to facilitate themselves. They have a happy and vibrant inner life that few get to share or see. They are deeply caring about others and don’t like to see people become unhappy.\n" + 
+			"\n" + 
+			"They are tremendously dedicated individuals, staying with companies for decades where others move from one job to the next. So, too, with relationships. Others may find Pisces difficult to get to know because they don’t often readily share their underlying passion and compassion. Pisces signs tend to be highly and deeply religious, and sometimes will go overboard with their devotion to their particular faith. This can also lead to intolerance and bigotry.\n" + 
+			"\n" + 
+			"Upside: Pisces are empathetic people, often feeling badly for someone whose life is not going well. They feel the need to reach out to those who are less fortunate and will often find themselves involved in drama that they hadn’t intended. They are kind, caring individuals who feel everything deeply, even though you would not know it on the surface. They can be creative and imaginative when they believe that they have support to make those leaps.\n" + 
+			"\n" + 
+			"Downside: Because they feel everything so intensely, this sign can often become a clinical worrier. They will worry that they are not worrying. This tendency to worry also leads to indecision on their part, fearing that if they make a decision that others disagree with that they will have upset someone. Pisces don’t like to hurt people’s feelings and as such become conflicted when they have to make decisions. They do not do well as leaders, but are impressive support personnel. Sometimes the have a lack of follow through due to this indecisiveness and require a gentle nudge from other signs to complete tasks on time.\n" + 
+			"\n" + 
+			"Pisces is a water sign, like Cancer and Scorpio.");
+} 
+else if(sign .equals("aries")) {
+	JOptionPane.showMessageDialog(null, "Aries Zodiac Sign\n" + 
+			"Fire sign; ruled by Mars.\n" + 
+			"Courageous, energetic, willful, commanding, leading. Often leads when following would be best course of action.\n" + 
+			"\n" + 
+			"→ Click here to read your Aries horoscope!\n" + 
+			"\n" + 
+			"The symbol for this sign is that of a ram for good reason. Aries is full of life and possesses high energy, which makes this sign always looking for a challenge. The typical Aries person is full of vitality, curiosity, and has a heightened sense of justice. They often excel at anything where a bit of competition is involved, whether this is of a cognitive nature or physical. The challenge is the thing. If there is a chance to win, to best someone, to show off abilities, Aries is in it to win it. The Aries person feels most alive when they are in control and leading others. They are often impatient and abrupt with those whom they feel are not their equals and are in a leadership position. Most Aries do not like being told what to do if the person in charge is less talented than they.\n" + 
+			"\n" + 
+			"Aries is a fire sign which means that they tend to have the characteristics of fire: warm, vital, and bright. They can sometimes be impulsive and have hot tempters. However, they have a deep desire to protect and when they have harmed someone in error they will be the first to make amends. They are optimistic and are not given to depressive interludes (which makes them also intolerant of those that do).\n" + 
+			"\n" + 
+			"Upside: Highly energetic, and has no problem working long hours on a single project. Often take the initiative experiencing new breakthroughs and discoveries. Aries are thorough, painstaking, and have a knack for precise records and clarity. They live hard, love hard, and play hard.\n" + 
+			"\n" + 
+			"Downside: Aries have tunnel vision when working on a project which can make them seem selfish. They can become impatient with those who are not similar to themselves temperamentally and intellectually. The dedication to a project can become stubbornness, possessiveness, and can lead to a withdrawn and sullen attitude. Many Aries who do not feel acknowledged for their accomplishments become sarcastic and rude.\n" + 
+			"\n" + 
+			"Other fire signs include Leo and Sagittarius.");
+}
+}
+}
 
-}
-}
+     
+      
