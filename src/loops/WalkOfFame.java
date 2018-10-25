682f701d7f57475031b2aa9ff39b861907a2c33d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-25 12:58:17 -0700

    fizz Buzz, Walk of fame

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -8,15 +8,40 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
-		Robot rob = new Robot();
+		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
 
 		Robot r2d2 = new Robot();
+		r2d2.penDown();
+		r2d2.setSpeed(100);
 		// 2. Make the robot draw a star shape. Hint: 144.
-
+		for (int i = 0; i < 5; i++) {
+			r2d2.move(30);
+			r2d2.turn(144);
+		}
+		r2d2.penUp();
+		r2d2.move(100);
+		r2d2.penDown();
+		// 2. Make the robot draw a star shape. Hint: 144.
+		for (int i = 0; i < 5; i++) {
+			r2d2.move(30);
+			r2d2.turn(144);
+		}
+		r2d2.penUp();
+		r2d2.move(100);
+		r2d2.penDown();
+		// 2. Make the robot draw a star shape. Hint: 144.
+		for (int i = 0; i < 5; i++) {
+			r2d2.move(30);
+			r2d2.turn(144);
+		}
+		r2d2.penUp();
+		r2d2.move(100);
+		r2d2.hide();
 		// 3. Set the size of the star to 30.
 
+		
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
