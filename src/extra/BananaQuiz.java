1e6f4eed8cd76e775d72327fbf3a4e60adcf8182
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-06 13:23:06 -0800

    obedient robot, bananna quiz

--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -4,23 +4,30 @@ package extra;
 
 import javax.swing.JOptionPane;
 
-public class BananaQuiz
-{
-	
-	public static void main(String[] args)
-	{
-		//1. ask the user if they like bananas
-		String answer = JOptionPane.
-		//2. if they say no, 
-			//tell them they are crazy 
-			//and end quiz
-		//3. if they say yes
-		//	ask them what is their favorite hobby
-		//	show a pop up that says "<your hobby> is much better with bananas!"
+public class BananaQuiz {
 
-		//4. OPTIONAL: if they say something other than “yes�? or “no�?
-		//	show a pop up that says “You are bananas!�?
-	
+	public static void main(String[] args) {
+		// 1. ask the user if they like bananas
+		String answer = JOptionPane.showInputDialog("Do you like banannas?");
+		// 2. if they say no,
+		// tell them they are crazy
+		if (answer.equals("no")) {
+			JOptionPane.showMessageDialog(null, "You're crazy?!");
+			JOptionPane.showMessageDialog(null, "You failed the quiz and your family, congrats!  :(");
+		}
+		// and end quiz
+		// 3. if they say yes
+		// ask them what is their favorite hobby
+		// show a pop up that says "<your hobby> is much better with bananas!"
+		else if (answer.equals("yes")) {
+			String hobby = JOptionPane.showInputDialog("What's your hobby?");
+			JOptionPane.showMessageDialog(null, hobby + " is much better with banannas!");
+		}
+		// 4. OPTIONAL: if they say something other than “yes�? or “no�?
+		// show a pop up that says “You are bananas!�?
+		else {
+			JOptionPane.showMessageDialog(null, "You are banannas!");
+		}
 	}
 
 }
---
+++ src/loops/ObedientRobot.java
@@ -0,0 +1,13 @@
+package loops;
+
+import javax.swing.JOptionPane;
+
+public class ObedientRobot {
+public static void main(String[] args) {
+	robot r2d2 = 
+	String shape = JOptionPane.showInputDialog("What shape do you want?");
+}
+  if(shape.equals("square")) {
+	  
+  }
+}
